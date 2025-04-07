5552caaed3442321d71cb682fb7dd5d36c0f5bd1
Daniel Tilleria <daniel.tilleria@hotmail.com>
2025-04-07 21:57:42 +0200

    chore: Remove unused Jest configuration from jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -8,23 +8,3 @@ const customJestConfig = {
 };
 
 module.exports = createJestConfig(customJestConfig);
-
-// v2
-
-// import "@testing-library/jest-dom";
-// const nextJest = require("next/jest");
-
-// const createJestConfig = nextJest({
-//   dir: "./",
-// });
-
-// const customJestConfig = {
-//   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-//   moduleNameMapper: {
-//     "^@/components/(.*)$": "<rootDir>/components/$1",
-//     ".(css|less|scss|sass)$": "identity-obj-proxy",
-//   },
-//   testEnvironment: "jsdom",
-// };
-
-// module.exports = createJestConfig(customJestConfig);
